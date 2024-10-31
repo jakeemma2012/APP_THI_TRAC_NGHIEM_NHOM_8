b60dafa6e59f21679e6aec00f4be012023b66b46
loiNguyen <loi.nguyenbao02@gmail.com>
2024-10-31 23:05:30 +0700

    sửa lỗi form và design form quanly , thisinh

---
+++ APP_THI_TRAC_NGHIEM/APP_THI_TRAC_NGHIEM_BUS/Services.cs
@@ -0,0 +1,61 @@
+﻿using APP_THI_TRAC_NGHIEM_BUS;
+using APP_THI_TRAC_NGHIEM_DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace App_Thi_Trac_Nghiem_BUS
+{
+    public class Services
+    {
+        public Model1 context = new Model1();
+
+        public static Services instance;
+
+        public Account account;
+
+        public static Services getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Services();
+            }
+            return instance;
+        }
+
+
+        public List<Status> getAllStatus()
+        {
+            return context.Status.ToList();
+        }
+
+        public List<Subject> getAllSubjects()
+        {
+            return context.Subjects.ToList();   
+        }
+
+        public List<Subject> getSubjectById(int id)
+        {
+            return context.Subjects.Where(p=>p.SubjectID == id).ToList();
+        }
+
+
+        public void SetAccountLogin(Account acc)
+        {
+            if (acc != null)
+            {
+                this.account = acc;
+            } else
+            {
+                MessageBox.Show("Xảy ra sự cố đăng nhập, vui lòng liên hệ lại bộ phận phát triển !");
+                Environment.Exit(0);
+            }
+
+        }
+        
+
+    }
+}
---
+++ APP_THI_TRAC_NGHIEM/APP_THI_TRAC_NGHIEM_BUS/StudentServices.cs
@@ -0,0 +1,37 @@
+﻿using APP_THI_TRAC_NGHIEM_DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Thi_Trac_Nghiem_BUS
+{
+    public class StudentServices
+    {
+        public Model1 context = Services.getInstance().context;
+        public static StudentServices instance;
+
+        public static StudentServices gI()
+        {
+            if (instance == null)
+                instance = new StudentServices();
+            return instance;
+        }
+
+        public List<Student> getAllStudent()
+        {
+            return context.Students.ToList();
+        }
+
+        public Student getStudenByUserName(int id)
+        {
+            return context.Students.FirstOrDefault(p => p.UserID == id);
+        }
+
+        public List<Student> getStudenByMSSV(string id)
+        {
+            return context.Students.Where(p => p.StudentID.Contains(id)).ToList();
+        }
+    }
+}
